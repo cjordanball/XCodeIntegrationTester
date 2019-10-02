46a3f95f423fac334b56f08b7e9c5f23a9a3ed8a
Jordan Ball <jordan.ball@willowtreeapps.com>
2019-10-02 15:00:35 -0400

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Jordan Ball on 10/2/19.
+//  Copyright © 2019 Ball Web Development, LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
